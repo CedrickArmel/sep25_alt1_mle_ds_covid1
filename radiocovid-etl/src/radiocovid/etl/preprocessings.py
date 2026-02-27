fix: fixed duplication between graycoprops and manual feature computation

radiocovid-etl/src/radiocovid/etl/preprocessings.py
@@ -1,5 +1,27 @@
 # MIT License
 #
+# Copyright (c) 2025 @CedrickArmel, @samarita22, @TaxelleT & @Yeyecodes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# MIT License
+#
 # Copyright (c) 2026 @CedrickArmel, @TaxelleT, @Yeyecodes & @samarita22
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -25,7 +47,6 @@ from typing import Any
 import cv2
 import numpy as np
 from joblib import Parallel, delayed
-from scipy.stats import entropy
 from skimage.feature import graycomatrix, graycoprops
 
 
@@ -175,15 +196,6 @@ def extract_texture_features_glcm(
     for ft in features:
         props = graycoprops(glcm, prop=ft)
         feature_values.append(props.mean())
-
-    # Mean / std / entropy sur pixels poumons uniquement
-    mean_val = np.mean(pixels)
-    std_val = np.std(pixels)
-
-    hist, _ = np.histogram(pixels, bins=256, range=(0, 256), density=True)
-    entropy_val = entropy(hist + 1e-10)
-
-    feature_values.extend([mean_val, std_val, entropy_val])
 
     return feature_values
 