feat: added train loss averaging, fixed best valscore stiching to current epoch valscore, refactored configs for modularity

radiocovid-core/src/radiocovid/core/models/__init__.py
@@ -32,7 +32,7 @@ import torch
 from torch.nn import Module
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler
-from torchmetrics import MaxMetric, Metric
+from torchmetrics import MaxMetric, Metric, MinMetric, MeanMetric
 from torchmetrics.utilities import dim_zero_cat
 
 from radiocovid.core.utils import RankedLogger
@@ -59,6 +59,16 @@ class LModule(L.LightningModule):
         self.val_score = copy.deepcopy(metric)
         self.test_score = copy.deepcopy(metric)
         self.best_val_score = MaxMetric(
+            process_group=metric.process_group,
+            compute_on_cpu=metric.compute_on_cpu,
+            sync_on_compute=metric.sync_on_compute,
+        )
+        self.train_loss = MeanMetric(
+            process_group=metric.process_group,
+            compute_on_cpu=metric.compute_on_cpu,
+            sync_on_compute=metric.sync_on_compute,
+        )
+        self.best_train_score = MinMetric(
             process_group=metric.process_group,
             compute_on_cpu=metric.compute_on_cpu,
             sync_on_compute=metric.sync_on_compute,
@@ -167,14 +177,18 @@ class LModule(L.LightningModule):
         self.output = []
         self._shared_start()
 
+    def on_train_epoch_start(self):
+        self.train_loss.reset()
+
     def training_step(
         self, batch: "dict[str, Any]", batch_idx: "int"
     ) -> "torch.Tensor":
         output_dict = self._model_step(batch)
         loss: "torch.Tensor" = output_dict["loss"]
+        self.train_loss.update(loss)
         self.log(
             "train_loss",
-            loss,
+            self.train_loss.compute(),
             on_step=True,
             on_epoch=True,
             logger=True,
@@ -189,7 +203,7 @@ class LModule(L.LightningModule):
         """Operates on a single batch of data from the validation set"""
         with torch.no_grad():
             loss, logits = self._shared_eval_step(batch)
-        self.val_score(logits.argmax(1), batch["target"])
+        self.val_score.update(logits.argmax(1), batch["target"])
         self.log_dict(
             dict(val_loss=loss, val_score=self.val_score),
             on_step=False,
@@ -203,7 +217,7 @@ class LModule(L.LightningModule):
     def test_step(self, batch: "dict[str, Any]", batch_idx: "int"):
         with torch.no_grad():
             loss, logits = self._shared_eval_step(batch)
-        self.test_score(logits.argmax(1), batch["target"])
+        self.test_score.update(logits.argmax(1), batch["target"])
         self.log_dict(
             dict(test_loss=loss, test_score=self.test_score),
             on_step=False,
@@ -226,7 +240,7 @@ class LModule(L.LightningModule):
         return logits
 
     def on_train_batch_end(self, outputs, batch, batch_idx) -> "None":
-        params = [p for p in self.parameters() if p.grad is not None]
+        params = list(self.parameters())
         if len(params) == 0:
             total_norm = torch.tensor(0.0)
         else:
@@ -243,42 +257,33 @@ class LModule(L.LightningModule):
             prog_bar=False,
             sync_dist=True,
         )
+    
+    def on_train_epoch_end(self):
+        self.best_train_loss.update(self.trainer.callback_metrics["train_loss_epoch"])
+        self.log(
+            "best_train_loss",
+            self.best_train_loss.compute(),
+            on_step=False,
+            on_epoch=True,
+            logger=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
 
     def on_validation_epoch_end(self) -> "None":
         """Called after the epoch ends to agg preds and logging"""
-        val_score = self.val_score.compute()
-        self.best_val_score(val_score)
+        self.best_val_score.update(self.val_score.compute())
         self.log(
             "best_val_score",
-            self.best_val_score,
+            self.best_val_score.compute(),
             on_step=False,
             on_epoch=True,
             logger=True,
             prog_bar=True,
             sync_dist=True,
         )
-        self.log(
-            "val_score",
-            val_score,
-            on_step=False,
-            on_epoch=True,
-            logger=True,
-            prog_bar=True,
-            sync_dist=True,
-        )
-        self.val_score.reset()
 
     def on_test_epoch_end(self) -> None:
-        test_score = self.test_score.compute()
-        self.log(
-            "test_score",
-            test_score,
-            on_step=False,
-            on_epoch=True,
-            logger=True,
-            prog_bar=True,
-            sync_dist=True,
-        )
         output: "torch.Tensor" = dim_zero_cat(x=self.output)
         if self.trainer.log_dir:
             torch.save(
@@ -287,7 +292,6 @@ class LModule(L.LightningModule):
                     self.trainer.log_dir, f"preds-rank{self.trainer.global_rank}.pt"  # type: ignore[arg-type]
                 ),
             )
-        self.test_score.reset()
 
     def _model_step(self, batch: dict[str, Any]) -> "dict[str, Any]":
         logits = self(batch["input"])